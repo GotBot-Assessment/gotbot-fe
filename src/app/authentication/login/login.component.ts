1077666f64d8a4b5dd45b183fdb9f136e848ff99
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 17:31:16 +0200

    Added toaster lib

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
+import { scrollToError } from '@gotbot-chef/shared/helpers/scroll-helper';
+import { validateAllFormFields } from '@gotbot-chef/shared/helpers/validators';
+import { LoadingStateService } from '@gotbot-chef/shared/services/ui/loading-state.service';
+import { finalize, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'gotbot-chef-login',
@@ -9,9 +14,26 @@ import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-obs
   templateUrl: './login.component.html',
   styles: ``
 })
-export class LoginComponent extends HasObservablesDirective{
+export class LoginComponent extends HasObservablesDirective {
   public readonly loginForm = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, [Validators.required, Validators.minLength(6)])
   });
+  private readonly loadingStateService = inject(LoadingStateService);
+  private readonly httpClient = inject(HttpClient);
+
+  public login(): void {
+    if (this.loginForm.invalid) {
+      validateAllFormFields(this.loginForm);
+
+      return scrollToError();
+    }
+
+    this.loadingStateService.start('processing');
+    this.httpClient.post('/gotbot/auth/login', this.loginForm.value)
+      .pipe(
+        finalize(() => this.loadingStateService.end('processing')),
+        takeUntil(this.destroy$)
+      ).subscribe();
+  }
 }
--- src/app/shared/interceptors/url-adjuster.interceptors.ts
+++ src/app/shared/interceptors/url-adjuster.interceptors.ts
@@ -4,7 +4,7 @@ import { environment } from '@environments/environment';
 export const urlAdjusterInterceptor: HttpInterceptorFn = (req, next) => {
   if (req.url.startsWith('/gotbot') && environment.production) {
     req = req.clone({
-      url: `${ environment.apiUrl }${ req.url.replace('gotbot/', '') }`
+      url: `${ environment.apiUrl }${ req.url.replace('gotbot/', 'api/') }`
     });
   }
 
