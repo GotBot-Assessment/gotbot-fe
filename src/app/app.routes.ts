2bc4845af5177d829483e4ebe82cc36f3d4041e1
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 19:23:25 +0200

    Routing to dashborad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authenticationRoutes } from '@gotbot-chef/authentication/authentication.routes';
+import { dashboardRoutes } from '@gotbot-chef/dashboard/dashboard.routes';
 import { authenticationGuard } from '@gotbot-chef/shared/guards/authentication.guard';
 
 export const routes: Routes = [
@@ -10,6 +11,10 @@ export const routes: Routes = [
   }, {
     path: 'authentication',
     children: authenticationRoutes,
-    canActivate: [authenticationGuard(false, '/dashboard')],
+    canActivate: [authenticationGuard(false, '/dashboard')]
+  }, {
+    path: 'dashboard',
+    children: dashboardRoutes,
+    canActivate: [authenticationGuard(true, '/authentication')]
   }
 ];
