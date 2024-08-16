737b9f084e25cd9d83d8d5d8b407aebde11d44b6
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 15:48:11 +0200

    Initial app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withComponentInputBinding, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from '@gotbot-chef/app.routes';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+      withHashLocation()
+    ),
+    provideAnimations(),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([])
+    ),
+    importProvidersFrom(ModalModule.forRoot())
+  ]
 };
