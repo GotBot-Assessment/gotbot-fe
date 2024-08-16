3c6beeaba46dd9d721f15493a7f095e56e9ee638
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 18:37:36 +0200

    wip: reg home

--- src/app/authentication/registration/registration.component.ts
+++ src/app/authentication/registration/registration.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'gotbot-chef-registration',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './registration.component.html',
   styles: ``
 })
