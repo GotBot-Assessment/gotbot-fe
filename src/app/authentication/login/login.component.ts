6d13319a8494a2f5c6f9c092541bb574dae18a1d
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 18:52:58 +0200

    Abstracted creating profile

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,18 +1,9 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
+import { AuthenticatesUserDirective } from '@gotbot-chef/authentication/authenticates-user.directive';
 import { LoadingStateDirective } from '@gotbot-chef/shared/drirectives/loading-state.directive';
-import { scrollToError } from '@gotbot-chef/shared/helpers/scroll-helper';
-import { validateAllFormFields } from '@gotbot-chef/shared/helpers/validators';
-import { UserModel } from '@gotbot-chef/shared/models/user.model';
-import { TokenService } from '@gotbot-chef/shared/services/token.service';
-import { LoadingStateService } from '@gotbot-chef/shared/services/ui/loading-state.service';
-import { UserProfileService } from '@gotbot-chef/shared/services/user-profile.service';
 import { FormInputComponent } from '@gotbot-chef/shared/ui/form-input/form-input.component';
-import { ToastrService } from 'ngx-toastr';
-import { finalize, switchMap, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'gotbot-chef-login',
@@ -26,43 +17,10 @@ import { finalize, switchMap, takeUntil, tap } from 'rxjs';
   templateUrl: './login.component.html',
   styles: ``
 })
-export class LoginComponent extends HasObservablesDirective {
-  public readonly loginForm = new FormGroup({
+export class LoginComponent extends AuthenticatesUserDirective{
+  public readonly authForm = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, [Validators.required, Validators.minLength(6)])
   });
-  private readonly loadingStateService = inject(LoadingStateService);
-  private readonly httpClient = inject(HttpClient);
-  private readonly toastr = inject(ToastrService);
-  private readonly tokenService = inject(TokenService);
-  private readonly userProfileService = inject(UserProfileService);
-
-  public login(): void {
-    if (this.loginForm.invalid) {
-      validateAllFormFields(this.loginForm);
-
-      return scrollToError();
-    }
-
-    this.loadingStateService.start(['processing', 'login']);
-    this.httpClient.post<{ token: string }>('/gotbot/auth/login', this.loginForm.value)
-      .pipe(
-        tap(res => this.tokenService.setToken(res.token)),
-        switchMap(() => this.httpClient.get<UserModel>('/gotbot/user')),
-        finalize(() => this.loadingStateService.end(['processing', 'login'])),
-        takeUntil(this.destroy$)
-      )
-      .subscribe({
-        next: (user) => {
-          this.userProfileService.setUser(user);
-
-          return window.location.reload();
-        },
-        error: (err) => {
-          this.loginForm.controls.password.setValue(null);
-
-          return this.toastr.error(err.error?.message ?? err.message ?? 'Invalid credentials', 'Login failed');
-        }
-      });
-  }
+  public readonly authUrl = '/gotbot/auth/login';
 }
--- src/app/authentication/registration/registration.component.ts
+++ src/app/authentication/registration/registration.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
+import { AuthenticatesUserDirective } from '@gotbot-chef/authentication/authenticates-user.directive';
+import { LoadingStateDirective } from '@gotbot-chef/shared/drirectives/loading-state.directive';
 import { FormInputComponent } from '@gotbot-chef/shared/ui/form-input/form-input.component';
 
 @Component({
@@ -10,15 +11,17 @@ import { FormInputComponent } from '@gotbot-chef/shared/ui/form-input/form-input
   imports: [
     RouterLink,
     FormInputComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LoadingStateDirective
   ],
   templateUrl: './registration.component.html'
 })
-export class RegistrationComponent extends HasObservablesDirective {
-  public readonly registrationForm = new FormGroup({
+export class RegistrationComponent extends AuthenticatesUserDirective {
+  public readonly authForm = new FormGroup({
     name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
     password_confirmation: new FormControl(null, [Validators.required, Validators.minLength(6)])
   });
+  public readonly authUrl = '/gotbot/auth/register';
 }
