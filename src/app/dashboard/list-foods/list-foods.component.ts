eaedf2104e938e893ab2dc24a52b49089f670783
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 20:27:28 +0200

    Added paginated response

--- src/app/dashboard/list-foods/list-foods.component.ts
+++ src/app/dashboard/list-foods/list-foods.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { afterNextRender, Component, inject } from '@angular/core';
+import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
+import { PaginatedResponse } from '@gotbot-chef/shared/models/api.response';
+import { ToastrService } from 'ngx-toastr';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'gotbot-chef-list-foods',
   standalone: true,
   imports: [],
   templateUrl: './list-foods.component.html',
-  styles: ``
 })
-export class ListFoodsComponent {
+export class ListFoodsComponent extends HasObservablesDirective {
+  private readonly httpClient = inject(HttpClient);
+  private readonly toastrService = inject(ToastrService);
 
+  public constructor() {
+    super();
+
+    afterNextRender(() => this.getMeals());
+  }
+
+  private getMeals(): void {
+    this.httpClient.get<PaginatedResponse<unknown>>('/gotbot/foods')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => this.toastrService.error(error.error?.message ?? error.message, 'Error')
+      });
+  }
 }
