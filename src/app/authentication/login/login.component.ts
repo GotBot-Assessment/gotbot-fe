c6e0d48923db4b7b0daf39491d829bf67922673d
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 17:27:07 +0200

    Added login form

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
 
 @Component({
   selector: 'gotbot-chef-login',
@@ -7,6 +9,9 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styles: ``
 })
-export class LoginComponent {
-
+export class LoginComponent extends HasObservablesDirective{
+  public readonly loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+  });
 }
