29de973794f547f744cafda00d7ae145c34fd609
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 15:52:20 +0200

    Added loading state service

---
+++ src/app/shared/drirectives/loading-state.directive.ts
@@ -0,0 +1,52 @@
+import { AfterViewInit, Directive, ElementRef, inject, input, OnDestroy } from '@angular/core';
+import { LoadingStateService } from '@gotbot-chef/shared/services/loading-state.service';
+import { Subscription } from 'rxjs';
+
+@Directive({
+  selector: '[gotbotChefLoadingState]',
+  standalone: true
+})
+export class LoadingStateDirective implements AfterViewInit, OnDestroy {
+  public aliasList = input.required<string | string[]>({
+    alias: 'gotbotChefLoadingState'
+  });
+
+  private stateSub!: Subscription;
+
+  private readonly element = inject(ElementRef);
+  private readonly loadingStateService = inject(LoadingStateService);
+
+  public ngAfterViewInit(): void {
+    this.stateSub = this.loadingStateService.on(this.aliasList()).subscribe({
+      next: change => change ? this.loadingOn() : this.loadingOff()
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.stateSub?.unsubscribe();
+  }
+
+  /**
+   * Disables the input and add the 'loading-state' class.
+   */
+  private loadingOn(): void {
+    if (this.element.nativeElement.disabled) {
+      this.element.nativeElement.classList.add('previously-disabled');
+    }
+    this.element.nativeElement.setAttribute('disabled', true);
+    this.element.nativeElement.classList.add('loading-state');
+  }
+
+  /**
+   * Enables the input and remove the 'loading-state' class.
+   */
+  private loadingOff(): void {
+    if (this.element.nativeElement.classList.contains('previously-disabled')) {
+      this.element.nativeElement.classList.remove('previously-disabled');
+
+      return this.element.nativeElement.classList.remove('loading-state');
+    }
+    this.element.nativeElement.removeAttribute('disabled');
+    this.element.nativeElement.classList.remove('loading-state');
+  }
+}
---
+++ src/app/shared/services/loading-state.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingStateService {
+  public loadingMessage = 'Please wait...';
+
+  /**
+   * Replay the last 10 loading states to ensure components are up to date with their correct loading state when subscribe
+   * @type {ReplaySubject<string>}
+   */
+  private readonly loadingStates$ = new ReplaySubject<{ alias: string; state: boolean }>(10);
+
+  /**
+   * @readonly
+   * @type {Observable<{ alias: string; state: boolean }>}
+   */
+  public get loadingStates(): Observable<{ alias: string; state: boolean }> {
+    return this.loadingStates$.asObservable();
+  }
+
+  /**
+   * Starts the loading service and disable inputs.
+   *
+   * @param {string} alias
+   * @param message
+   */
+  public start(alias: string | string[], message = 'Please wait ...'): void {
+    alias = alias instanceof Array ? alias : [alias];
+
+    //assign loader message.
+    this.loadingMessage = message;
+
+    alias.forEach(alias => {
+      this.loadingStates$.next({
+        alias,
+        state: true
+      });
+    });
+  }
+
+  /**
+   * Ends the loading service and enable the inputs.
+   *
+   * @param {string} alias
+   */
+  public end(alias: string | string[]): void {
+    alias = alias instanceof Array ? alias : [alias];
+
+    alias.forEach(alias => {
+      this.loadingStates$.next({
+        alias,
+        state: false
+      });
+    });
+  }
+
+  /**
+   * helper method to filter listening events to those required
+   * @param {string} alias
+   * @returns {Observable<boolean>}
+   */
+  public on(alias: string | string[]): Observable<boolean> {
+    alias = alias instanceof Array ? alias : [alias];
+
+    return this.loadingStates.pipe(
+      filter((stateChange: { alias: string; state: boolean }) => alias.includes(stateChange.alias)),
+      map(stateChange => stateChange.state)
+    );
+  }
+}
