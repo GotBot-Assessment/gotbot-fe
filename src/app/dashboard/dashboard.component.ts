bdaa6450e5a37a6c3e8a818373968013a3081f74
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 20:09:12 +0200

    Removed side nav component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideNavComponent } from '@gotbot-chef/shared/ui/side-nav/side-nav.component';
 
 @Component({
   selector: 'gotbot-chef-dashboard',
   standalone: true,
   imports: [
     RouterOutlet,
-    SideNavComponent
   ],
   templateUrl: './dashboard.component.html',
   styles: ``
