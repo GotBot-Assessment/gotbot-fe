9dcae87c85bef8a89b379e6221b39fcece13ecd9
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 17:35:42 +0200

    Setting token on login

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -4,9 +4,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
 import { scrollToError } from '@gotbot-chef/shared/helpers/scroll-helper';
 import { validateAllFormFields } from '@gotbot-chef/shared/helpers/validators';
+import { TokenService } from '@gotbot-chef/shared/services/token.service';
 import { LoadingStateService } from '@gotbot-chef/shared/services/ui/loading-state.service';
 import { ToastrService } from 'ngx-toastr';
-import { finalize, takeUntil } from 'rxjs';
+import { finalize, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'gotbot-chef-login',
@@ -23,6 +24,7 @@ export class LoginComponent extends HasObservablesDirective {
   private readonly loadingStateService = inject(LoadingStateService);
   private readonly httpClient = inject(HttpClient);
   private readonly toastr = inject(ToastrService);
+  private readonly tokenService = inject(TokenService);
 
   public login(): void {
     if (this.loginForm.invalid) {
@@ -32,8 +34,9 @@ export class LoginComponent extends HasObservablesDirective {
     }
 
     this.loadingStateService.start('processing');
-    this.httpClient.post('/gotbot/auth/login', this.loginForm.value)
+    this.httpClient.post<{ token: string }>('/gotbot/auth/login', this.loginForm.value)
       .pipe(
+        tap(res => this.tokenService.setToken(res.token)),
         finalize(() => this.loadingStateService.end('processing')),
         takeUntil(this.destroy$)
       )
