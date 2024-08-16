bab9322fbd80f017d4685efbcb12c7a8a2e5ac27
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 17:21:09 +0200

    Added loader to screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpinnerService } from '@gotbot-chef/shared/services/ui/spinner.service';
 
 @Component({
   selector: 'gotbot-chef-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styles: [],
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'GotBotChef';
+  public readonly spinnerService = inject(SpinnerService);
+  private readonly cdr = inject(ChangeDetectorRef);
+
+  public constructor() {
+    effect(() => {
+      this.spinnerService.httpCallsCount();
+
+      return this.cdr.detectChanges();
+    });
+  }
 }
