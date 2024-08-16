474a7eee4b23c420829de093b2e259e06a068350
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 18:07:54 +0200

    added error pipe

---
+++ src/app/shared/ui/form-input/form-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gotbot-chef-form-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-input.component.html',
+  styles: ``
+})
+export class FormInputComponent {
+
+}
