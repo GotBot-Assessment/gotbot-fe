e7721ccb4a944c793db2b37ded70dfb40cbb000a
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 20:43:23 +0200

    Fetching meals

--- src/app/dashboard/list-foods/list-foods.component.ts
+++ src/app/dashboard/list-foods/list-foods.component.ts
@@ -1,18 +1,25 @@
 import { HttpClient } from '@angular/common/http';
-import { afterNextRender, Component, inject } from '@angular/core';
+import { afterNextRender, Component, effect, inject, signal, viewChild } from '@angular/core';
 import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
 import { PaginatedResponse } from '@gotbot-chef/shared/models/api.response';
 import { FoodModel } from '@gotbot-chef/shared/models/food.model';
+import { PageChangedEvent, PaginationComponent, PaginationModule } from 'ngx-bootstrap/pagination';
 import { ToastrService } from 'ngx-toastr';
 import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'gotbot-chef-list-foods',
   standalone: true,
-  imports: [],
+  imports: [
+    PaginationModule
+  ],
   templateUrl: './list-foods.component.html',
+  styles: [`:host {display: block}`]
 })
 export class ListFoodsComponent extends HasObservablesDirective {
+  public readonly pagination = viewChild(PaginationComponent);
+  public readonly paginatedFoods = signal<PaginatedResponse<FoodModel> | undefined>(undefined);
+  public readonly currentPage = signal(1);
   private readonly httpClient = inject(HttpClient);
   private readonly toastrService = inject(ToastrService);
 
@@ -20,15 +27,25 @@ export class ListFoodsComponent extends HasObservablesDirective {
     super();
 
     afterNextRender(() => this.getMeals());
+
+    effect(() => {
+      if (this.pagination() && this.currentPage()) {
+        this.pagination()!.page = this.currentPage();
+      }
+    });
+  }
+
+  public pageChanged(event: PageChangedEvent): void {
+    this.currentPage.set(event.page);
+    this.getMeals();
   }
 
   private getMeals(): void {
-    this.httpClient.get<PaginatedResponse<FoodModel>>('/gotbot/foods')
-      .pipe(takeUntil(this.destroy$))
+    this.httpClient.get<PaginatedResponse<FoodModel>>('/gotbot/foods', {
+      params: { page: this.currentPage() }
+    }).pipe(takeUntil(this.destroy$))
       .subscribe({
-        next: (response) => {
-          console.log(response);
-        },
+        next: (response) => this.paginatedFoods.set(response),
         error: (error) => this.toastrService.error(error.error?.message ?? error.message, 'Error')
       });
   }
--- src/app/shared/interceptors/url-adjuster.interceptors.ts
+++ src/app/shared/interceptors/url-adjuster.interceptors.ts
@@ -2,7 +2,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '@environments/environment';
 
 export const urlAdjusterInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log(req.url);
   if (req.url.startsWith('/gotbot')) {
     req = req.clone({
       url: `${ environment.apiUrl }${ req.url.replace('gotbot/', 'api/') }`
