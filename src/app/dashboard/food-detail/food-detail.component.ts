fce56caaf2358799e6a146ae345de027199b5359
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 19:20:24 +0200

    Added dashboard pages

---
+++ src/app/dashboard/food-detail/food-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gotbot-chef-food-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-detail.component.html',
+  styles: ``
+})
+export class FoodDetailComponent {
+
+}
---
+++ src/app/dashboard/list-foods/list-foods.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gotbot-chef-list-foods',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-foods.component.html',
+  styles: ``
+})
+export class ListFoodsComponent {
+
+}
