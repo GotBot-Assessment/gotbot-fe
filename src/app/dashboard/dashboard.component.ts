35f81b75975f77d5c27dbcd537597d9eec0c38ba
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 19:32:00 +0200

    Added side nav button

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNavComponent } from '@gotbot-chef/shared/ui/side-nav/side-nav.component';
 
 @Component({
   selector: 'gotbot-chef-dashboard',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    SideNavComponent
   ],
   templateUrl: './dashboard.component.html',
   styles: ``
---
+++ src/app/shared/ui/side-nav/side-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gotbot-chef-side-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-nav.component.html',
+  styles: ``
+})
+export class SideNavComponent {
+
+}
