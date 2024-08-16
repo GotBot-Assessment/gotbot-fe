08a7eb6e58f4024d3a67fa9f0521d860c884c5df
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 17:17:21 +0200

    Fixed auth guards

--- src/app/shared/guards/authentication.guard.ts
+++ src/app/shared/guards/authentication.guard.ts
@@ -1,10 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { TokenService } from '@gotbot-chef/shared/services/token.service';
+import { UserProfileService } from '@gotbot-chef/shared/services/user-profile.service';
 
 export const authenticationGuard = (shouldBeLoggedIn: boolean, redirectUrl: string): CanActivateFn => {
   return () => {
-    //TODO: check user authentication.
-    console.log(shouldBeLoggedIn, redirectUrl);
+    const user = inject(UserProfileService).getUser();
+    const token = inject(TokenService).getToken();
+    const router = inject(Router);
 
-    return true;
+    if (!!(user && token) === shouldBeLoggedIn) {
+      return true;
+    }
+
+    return !router.navigate([redirectUrl]) as unknown as false;
   };
 };
