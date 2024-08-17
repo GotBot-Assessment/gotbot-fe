7a90f7ef9f7e0d0e0a436cbbe41409333d82bb7a
Innocent Mazando <innocentmazando@gmail.com>
2024-08-17 10:34:46 +0200

    Cancelling modal

--- src/app/dashboard/save-food/save-food.component.ts
+++ src/app/dashboard/save-food/save-food.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'gotbot-chef-save-food',
   standalone: true,
   imports: [],
   templateUrl: './save-food.component.html',
-  styles: ''
 })
 export class SaveFoodComponent {
+  public readonly modalRef = inject(BsModalRef<SaveFoodComponent>);
   public readonly foodForm = new FormGroup({
     name: new FormControl(null, Validators.required),
     category: new FormControl(null, Validators.required),
@@ -16,6 +17,6 @@ export class SaveFoodComponent {
     price: new FormControl(null, Validators.required),
     description: new FormControl(null, Validators.required),
     file: new FormControl(null, Validators.required),
-    ingredients: new FormArray([])
+    ingredients: new FormArray([], Validators.required)
   });
 }
