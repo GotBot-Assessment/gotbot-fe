8b6987a37b8dd1fc6baea5b3aaeed6717c1ba9b1
Innocent Mazando <innocentmazando@gmail.com>
2024-08-17 09:16:53 +0200

    Linked logout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
+import { DialogService } from '@gotbot-chef/shared/services/ui/dialog.service';
 
 @Component({
   selector: 'gotbot-chef-dashboard',
@@ -11,6 +14,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styles: ''
 })
-export class DashboardComponent {
+export class DashboardComponent extends HasObservablesDirective {
+  private readonly httpClient = inject(HttpClient);
+  private readonly dialogService = inject(DialogService);
 
+  public logout(): void {
+    console.log(this.httpClient, this.dialogService);
+  }
 }
