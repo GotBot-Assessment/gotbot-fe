cd6fdfa2b22a0ccf11bfef72eec3b3dd3092a38c
Innocent Mazando <innocentmazando@gmail.com>
2024-08-17 08:35:19 +0200

    setting food item

--- src/app/dashboard/food-detail/food-detail.component.ts
+++ src/app/dashboard/food-detail/food-detail.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { afterNextRender, Component, inject, input, signal } from '@angular/core';
+import { HasObservablesDirective } from '@gotbot-chef/shared/drirectives/has-observables.directive';
+import { FoodModel } from '@gotbot-chef/shared/models/food.model';
+import { ToastrService } from 'ngx-toastr';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'gotbot-chef-food-detail',
   standalone: true,
   imports: [],
   templateUrl: './food-detail.component.html',
-  styles: ``
+  styles: ''
 })
-export class FoodDetailComponent {
+export class FoodDetailComponent extends HasObservablesDirective {
+  public readonly id = input.required<number>();
+  public readonly food = signal<FoodModel | undefined>(undefined);
+  private readonly httpClient = inject(HttpClient);
+  private readonly toasterService = inject(ToastrService);
 
+  public constructor() {
+    super();
+
+    afterNextRender(() => this.fetchFoodDetail());
+  }
+
+  private fetchFoodDetail(): void {
+    this.httpClient.get<FoodModel>('/gotbot/foods/' + this.id())
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: food => this.food.set(food),
+        error: (error) => this.toasterService.error(error.error?.message ?? error.message, 'Error')
+      });
+  }
 }
