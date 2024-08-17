d24002417122e3552d146973d851e27102c8d9d6
Innocent Mazando <innocentmazando@gmail.com>
2024-08-17 10:41:41 +0200

    Added category

--- src/app/dashboard/save-food/save-food.component.ts
+++ src/app/dashboard/save-food/save-food.component.ts
@@ -1,22 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormInputComponent } from '@gotbot-chef/shared/ui/form-input/form-input.component';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'gotbot-chef-save-food',
   standalone: true,
-  imports: [],
-  templateUrl: './save-food.component.html',
+  imports: [
+    ReactiveFormsModule,
+    FormInputComponent
+  ],
+  templateUrl: './save-food.component.html'
 })
 export class SaveFoodComponent {
   public readonly modalRef = inject(BsModalRef<SaveFoodComponent>);
   public readonly foodForm = new FormGroup({
     name: new FormControl(null, Validators.required),
     category: new FormControl(null, Validators.required),
-    area: new FormControl(null, Validators.required),
+    area: new FormControl(null),
     price: new FormControl(null, Validators.required),
     description: new FormControl(null, Validators.required),
     file: new FormControl(null, Validators.required),
     ingredients: new FormArray([], Validators.required)
   });
+  public readonly categories = [
+    'Starter',
+    'Main',
+    'Dessert'
+  ].map(name => ({ name }));
 }
